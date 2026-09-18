4362525277db740b6be4b851adf6a2903e1eb91a
laotu <2427796523@qq.com>
2026-09-18 14:39:21 +0800

    update preset

--- templates/root/疯狂水世界/tools/抽奖-0.script.js
+++ templates/root/疯狂水世界/tools/抽奖-0.script.js
@@ -1,1 +1,3 @@
-const jc=context.window._fkssj.jc(input.type);const item=context.window.sUtils.weightRandom(jc,i=>i[1]);return item[0]
+const jc = context.window._fkssj.jc(input.type);
+const item = context.window.sUtils.weightRandom(jc, (i) => i[1]);
+return item[0];
